76a405ec717edf0def2aa1ed9244bcf5077201d6
root <root@ip-172-31-47-39.us-east-2.compute.internal>
2019-08-20 12:29:38 +0000

    Change no of time

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Hello{
 	public static void main(String[] args){
-		for(int i = 0; i < 5; i++){
+		for(int i = 0; i < 7; i++){
 			System.out.println("Hello World!\n");
 		}
 	}
